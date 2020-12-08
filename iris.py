0038b7bf3a6441209cf1ec30f0d3f5cdee25f559
Yuto Suzuki <mokkumokku99@gmail.com>
2020-12-08 10:49:48 +0900

    show matrix

--- iris.py
+++ iris.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
 
 iris_dataset=load_iris()
 
-print(iris_dataset['data'])
+X_train,X_test,y_train,y_test =train_test_split(iris_dataset['data'],iris_dataset['target'],random_state=0)
+
+iris_dataframe=pd.DataFrame(X_train,columns=iris_dataset.feature_names)
+
+grr=pd.plotting.scatter_matrix(iris_dataframe,c=y_train)
+plt.show()
