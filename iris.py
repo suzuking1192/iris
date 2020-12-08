885a426596fe0cffd6a6695fccdabea29f1cd845
Yuto Suzuki <mokkumokku99@gmail.com>
2020-12-08 11:04:55 +0900

    random forest

--- iris.py
+++ iris.py
@@ -1,7 +1,8 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as  plt
+from sklearn.ensemble import RandomForestClassifier
 
 iris_dataset=load_iris()
 
@@ -9,5 +10,20 @@ X_train,X_test,y_train,y_test =train_test_split(iris_dataset['data'],iris_datase
 
 iris_dataframe=pd.DataFrame(X_train,columns=iris_dataset.feature_names)
 
-grr=pd.plotting.scatter_matrix(iris_dataframe,c=y_train)
-plt.show()
+#grr=pd.plotting.scatter_matrix(iris_dataframe,c=y_train)
+#plt.show()
+
+model = RandomForestClassifier(n_estimators=5,random_state=0)
+model.fit(X_train,y_train)
+
+print(model.score(X_test,y_test))
+
+# find the best number of trees
+
+i= 1
+while i <= 10:
+    testmodel=RandomForestClassifier(n_estimators=i,random_state=0)
+    testmodel.fit(X_train,y_train)
+    print("Accuracy : {} trees ={}".format(i,testmodel.score(X_test,y_test)))
+
+    i += 1
